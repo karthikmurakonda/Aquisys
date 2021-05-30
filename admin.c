06f8bd666f43ddda2b2f27f875ad2cab26c6cc4a
karthikmurakonda <karthik.murakonda14@gmail.com>
2021-05-30 22:57:26 +0530

    fixed few bugs

--- admin.c
+++ admin.c
@@ -5,11 +5,10 @@
     welcomepage_admin();
     return 0;
 }*/
-
 void preview_question(int quiz_id,int ques_id){
     int alts= 0;
     for (int alt = 0; alt < max_alternative_q; alt++){
-            if(quizlist.quiz[quiz_id].question[ques_id][alt].response[0].status == 'N') {
+            if(quizlist.quiz[quiz_id].question[ques_id][alt].response[0].status != 'E') {
                 printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%d.%d~~~~~~~~~~~~~~~~~~~~~~~~\n\n",ques_id+1,alt+1);
                 printf("Marks - %d\n\n",quizlist.quiz[quiz_id].question[ques_id][alt].marks);
                 printf("Statement:\n%s\n",quizlist.quiz[quiz_id].question[ques_id][alt].statement);
@@ -18,11 +17,9 @@ void preview_question(int quiz_id,int ques_id){
                 alts++;
             }
     }
-    printf("\n Type corresponding alt number to edit\notherwise type 0 to add alt questions\n Type -1 to go back to questions page\n");
+    printf("\n Type corresponding alt number to edit\notherwise type 0 to add alt questions/solution(you have to rewrite the total question and solution)\n Type -1 to go back to questions page\n");
     int res;
-    do {
-    scanf("%d",&res);
-    clearBuf();
+	res = scanf_int(alts,-1);
     if(res==0){
             if(alts != 5){
             add_question(quiz_id,ques_id,alts);
@@ -39,8 +36,6 @@ void preview_question(int quiz_id,int ques_id){
     else if (res == -1) {
         edit_questions(quiz_id);
     }
-    else printf("Not a valid response try agian\n");
-    }while(res < 0 && res >alts);
 }
 
 void preveiw_quiz(int quiz_Id){
@@ -50,7 +45,7 @@ void preveiw_quiz(int quiz_Id){
     for (int qid = 0; qid <quizlist.quiz[quiz_Id].no_of_questions; qid++){
         int no_of_alts= 0;
         for (int i = 0; i < max_alternative_q; i++){
-            if(quizlist.quiz[quiz_Id].question[qid][i].response[0].status == 'N') no_of_alts++;
+            if(quizlist.quiz[quiz_Id].question[qid][i].response[0].status != 'E') no_of_alts++;
         }        
         printf("     %d                           %d                   %d\n",qid+1,no_of_alts,quizlist.quiz[quiz_Id].question[qid][0].marks);    
     }
@@ -58,29 +53,23 @@ void preveiw_quiz(int quiz_Id){
 
 void add_question(int quiz_id,int question_id,int alt_id){
     clearscr();
-        int resp;
-        while(quizlist.quiz[quiz_id].question[question_id][0].marks == 0) {
+    int resp;
+    if (quizlist.quiz[quiz_id].question[question_id][0].marks == 0) {
         printf("Marks of %d question?\n",question_id+1);
-        scanf("%d",&resp);
-        clearBuf();
-        if(resp >0){
-            quizlist.quiz[quiz_id].question[question_id][0].marks = resp;
-        }
-        else printf("Not a valid response try agian\n");
-        }
-        for (int ip = 0; ip < max_alternative_q; ip++){
-            quizlist.quiz[quiz_id].question[question_id][ip].marks=quizlist.quiz[quiz_id].question[question_id][0].marks;
-        }
-        
-    
+        resp = scanf_int(1000,1);
+        quizlist.quiz[quiz_id].question[question_id][0].marks = resp;
+    }
+    for (int ip = 0; ip < max_alternative_q; ip++){
+        quizlist.quiz[quiz_id].question[question_id][ip].marks=quizlist.quiz[quiz_id].question[question_id][0].marks;
+    }	
     char check='n';
     while(check != 'y'){
         printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%d.%d~~~~~~~~~~~~~~~~~~~~~~~~\n\n",question_id+1,alt_id+1);
         printf("type statment of %d-alternate for %d question(%d marks)\n\n",alt_id+1,question_id+1,quizlist.quiz[quiz_id].question[question_id][0].marks);
         printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-        smart_fgets(quizlist.quiz[quiz_id].question[question_id][alt_id].statement,2000,stdin);
+        smart_fgets(quizlist.quiz[quiz_id].question[question_id][alt_id].statement,max_question_length,stdin);
         printf("\nType Solution:\n");
-        smart_fgets(quizlist.quiz[quiz_id].question[question_id][alt_id].solution,1000,stdin);
+        smart_fgets(quizlist.quiz[quiz_id].question[question_id][alt_id].solution,max_answer_length,stdin);
         printf("\npreview:\n\n%s\n\n",quizlist.quiz[quiz_id].question[question_id][alt_id].statement);
         printf("Solution:\n");
         printf("%s\n\n",quizlist.quiz[quiz_id].question[question_id][alt_id].solution);
@@ -103,42 +92,30 @@ void edit_questions(int quiz_id){
     preveiw_quiz(quiz_id);
     printf("Type ques_number to preview,edit or add alternate question numbers\nType 0 to create questions\nType -1 to exit\n");
     int res;
-    do {
-    scanf("%d",&res);
-    clearBuf();
-    if(res== -1){
+    res = scanf_int(quizlist.quiz[quiz_id].no_of_questions,-1);
+  	if(res== -1){
           admin_quizdetails(quiz_id);
     }
-    else if(res >0 && res <= quizlist.quiz[quiz_id].no_of_questions){
+	else if(res >0 && res <= quizlist.quiz[quiz_id].no_of_questions){
         preview_question(quiz_id,res-1);
-    }
-    else if(res == 0){  //adding questions.
-        printf("Type no.of question to added extra(you can add %d maximum)\n",200-quizlist.quiz[quiz_id].no_of_questions);
+	}
+    else if(res == 0){   //adding questions.
+        printf("Type no.of question to added extra(you can add %d maximum)\n",max_q_per_quiz-quizlist.quiz[quiz_id].no_of_questions);
         int res2=0;
-        do {
-            scanf("%d",&res2);
-                clearBuf();
-            if(res2 == 0){
-            continue;
-        }
-            else if(res2 > 0){
-                if (res2 <= 200-quizlist.quiz[quiz_id].no_of_questions){
-                quizlist.quiz[quiz_id].no_of_questions += res2;
-                ques_initialize(quizlist.quiz[quiz_id].no_of_questions-res2,quiz_id);
-                addquestions_initial(quizlist.quiz[quiz_id].no_of_questions-res2,quiz_id);
+	    res2 = scanf_int(max_q_per_quiz-quizlist.quiz[quiz_id].no_of_questions,0);
+  	        if(res2 == 0){
+                edit_questions(quiz_id);
                 return;
-                }
-                else{
-                     printf("too many questions");
-                     res2 = -1;
-                }
         }
-            else printf("Not a valid response try agian\n");
-        }while(res2< 0);
+        else if(res2 > 0){
+            if (res2 <= max_q_per_quiz-quizlist.quiz[quiz_id].no_of_questions){
+            quizlist.quiz[quiz_id].no_of_questions += res2;
+            ques_initialize(quizlist.quiz[quiz_id].no_of_questions-res2,quiz_id);
+            addquestions_initial(quizlist.quiz[quiz_id].no_of_questions-res2,quiz_id);
+            return;
+            }
+        }
     }
-    else printf("Not a valid response try agian\n");
-    }while(res <-1 || res> quizlist.quiz[quiz_id].no_of_questions);
-
 
 }
 
@@ -178,8 +155,8 @@ void addquestions_initial(int questart_id,int quiz_id){
             printf("You can still add(optional) %d no.of alternate questions would like to continue?(y/n)",diff);
  
             do {
-            scanf("%c",&res2);
-            clearBuf();
+	        scanf("%c",&res2);
+	        clearBuf();
             if(res2 == 'y' || res2 == 'Y'){
                 add_question(quiz_id,i,5-diff);
                 diff--;
@@ -190,7 +167,7 @@ void addquestions_initial(int questart_id,int quiz_id){
             }
             else printf("not a valid input\n");
 
-        }while(res2 != 'y'&& res2 != 'Y'  && res2!= 'n'&&res2!='N');
+	    }while(res2 != 'y'&& res2 != 'Y'  && res2!= 'n'&&res2!='N');
         }   
     }
     edit_questions(quiz_id);
@@ -198,50 +175,50 @@ void addquestions_initial(int questart_id,int quiz_id){
 }
 
 void addquiz(){
-    quizlist.no_of_quizes = quizlist.no_of_quizes+1;//increasing no.of quizes.
-    int i = quizlist.no_of_quizes - 1;//i is index for quiz array.
-    printf("Quiz name?\n");//taking quiz name.
-    char x[101];
-    smart_fgets(x,101,stdin);
-    strcpy(quizlist.quiz[i].name,x);
-    quizlist.quiz[i].no_of_students_attempted=0;//initialising to zero.
-    //no.of questions.
-    printf("no.of questions?(type number between 1 to 200)\n");
-    int res;
-    scanf("%d",&res);
-    clearBuf();
-    while(res <= 0 || res >= 200){
-        printf("invalid response type again(should be a number in range 1 to 200)\n");
-        scanf("%d",&res);
-        clearBuf();
-    }
-    quizlist.quiz[i].no_of_questions = res;
-    //no_of_max_attempts part.
-    printf("no.of maximum attepts?(type number between 1 to 5)\n");
-    scanf("%d",&res);
-    clearBuf();
-    while(res <= 0 || res >= 6){
-        printf("invalid response type again(should be a number in range 1 to 5)\n");
-        scanf("%d",&res);
-        clearBuf();
-    }
-    quizlist.quiz[i].no_of_max_attempts = res;
-    //conformation.
-    printf("you have to add atleast %d questions\nWould you like to continue?(y/n)\n",quizlist.quiz[i].no_of_max_attempts*quizlist.quiz[i].no_of_questions);
-    char res2;
-    do {
-    scanf("%c",&res2);
-    clearBuf();
-    if(res2=='y'||res2=='Y'){
+	quizlist.no_of_quizes = quizlist.no_of_quizes+1;//increasing no.of quizes.
+  	int i = quizlist.no_of_quizes - 1;//i is index for quiz array.
+	printf("Quiz name?\n");//taking quiz name.
+	char x[101];
+	smart_fgets(x,101,stdin);
+	strcpy(quizlist.quiz[i].name,x);
+	quizlist.quiz[i].no_of_students_attempted=0;//initialising to zero.
+	//no.of questions.
+	printf("no.of questions?(type number between 1 to 200)\n");
+	int res;
+	scanf("%d",&res);
+	clearBuf();
+	while(res <= 0 || res >= max_q_per_quiz){
+		printf("invalid response type again(should be a number in range 1 to 200)\n");
+		scanf("%d",&res);
+		clearBuf();
+	}
+	quizlist.quiz[i].no_of_questions = res;
+	//no_of_max_attempts part.
+	printf("no.of maximum attepts?(type number between 1 to 5)\n");
+	scanf("%d",&res);
+	clearBuf();
+	while(res <= 0 || res >= 6){
+		printf("invalid response type again(should be a number in range 1 to 5)\n");
+		scanf("%d",&res);
+		clearBuf();
+	}
+	quizlist.quiz[i].no_of_max_attempts = res;
+	//conformation.
+	printf("you have to add atleast %d questions\nWould you like to continue?(y/n)\n",quizlist.quiz[i].no_of_max_attempts*quizlist.quiz[i].no_of_questions);
+	char res2;
+	do {
+	scanf("%c",&res2);
+	clearBuf();
+  	if(res2=='y'||res2=='Y'){
         ques_initialize(0,quizlist.no_of_quizes-1);
-        addquestions_initial(0,i);
+	  	addquestions_initial(0,i);
     }
-    else if(res2=='n'||res2=='N'){
-        quizlist.no_of_quizes = quizlist.no_of_quizes-1;
-        showqlist_admin();
-    }
+	else if(res2=='n'||res2=='N'){
+		quizlist.no_of_quizes = quizlist.no_of_quizes-1;
+		showqlist_admin();
+	}
     else printf("Not a valid response try agian\n");
-    }while(res2 != 'n' && res2 != 'y' && res2!= 'N' && res2!='Y');
+	}while(res2 != 'n' && res2 != 'y' && res2!= 'N' && res2!='Y');
 }
 
 void response_admin(int quiz_id){
@@ -256,51 +233,49 @@ void response_admin(int quiz_id){
     }
     
     int res;
-    do {
-    scanf("%d",&res);
-    clearBuf();
-    if(res >= 0 && res <max_users*max_alternative_q){
+    res = scanf_int(max_users*max_alternative_q-1,0);
+  	if(res >= 0 && res <max_users*max_alternative_q){
           //abhishek's function
     }
     else printf("Not a valid response try agian\n");
-    }while(res < 0 || res >= max_users*max_alternative_q);
     }
     else {
-        printf("no one attempted the quiz");
+        printf("no one attempted the quiz\nPress ENTER to goback");
+        getchar();
         admin_quizdetails(quiz_id);
     }
 
 }
 
- void change_marks(int quiz_id){
+void change_marks(int quiz_id){
     printf("Type the question number to change marks of that question\nType 0 to goback\n");
     int res;
-int check = 0;
-    do {
-    scanf("%d",&res);
-    clearBuf();
-    if(res == 0  ){
+    int check = 0;
+	do {
+	scanf("%d",&res);
+	clearBuf();
+  	if(res == 0  ){
          admin_quizdetails(quiz_id);
          check =1;
     }
-    else if(res <= quizlist.quiz[quiz_id].no_of_questions && res >0){
+	else if(res <= quizlist.quiz[quiz_id].no_of_questions && res >0){
         printf("type new marks for question %d(previously %d marks)\n",res,quizlist.quiz[quiz_id].question[res-1][0].marks);
         int res2;
         check = 1;
         int check2 = 0;
-        do {
-            scanf("%d",&res2);
-            clearBuf();
-            if(res2 >0 ){
+	    do {
+	        scanf("%d",&res2);
+	        clearBuf();
+  	        if(res2 >0 ){
                 for(int i = 0;i <max_alternative_q;i++) quizlist.quiz[quiz_id].question[res-1][i].marks = res2; 
             check2 = 1;
             admin_quizdetails(quiz_id);
             }
             else printf("Not a valid response try agian\n");
-        }while(check2 == 0);
-    }
+	    }while(check2 == 0);
+	}
     else printf("Not a valid response try agian\n");
-    }while(check == 0);
+	}while(check == 0);
  }
 
 void admin_quizdetails(int n){
@@ -311,17 +286,17 @@ void admin_quizdetails(int n){
     printf("Type\n r : to view student responses\n m : To change distrubution of marks\n e : To add questions/edit existing questions \n 0 : to go back to quizzes list\n");
     char res;
     int dm = 0;
-    do {
-    scanf("%c",&res);
-    clearBuf();
-    if(res=='r'){
+	do {
+	scanf("%c",&res);
+	clearBuf();
+  	if(res=='r'){
         response_admin(n);
         dm = 1;
     }
-    else if(res == 'e'){
+	else if(res == 'e'){
         edit_questions(n);
         dm = 1;
-    }
+	}
     else if(res == 'm'){
         preveiw_quiz(n);
         change_marks(n);
@@ -332,31 +307,31 @@ void admin_quizdetails(int n){
         dm = 1;
     }
     else printf("Not a valid response try agian\n");
-    }while(dm != 1);
+	}while(dm != 1);
 }
 
-void showqlist_admin() {
+void showqlist_admin(){
     clearscr();
     printf("type a number to veiw/edit quiz or type '0' to create a quiz\npress '-1' to go back to welcome page\n" );
     for (int  i = 0; i < quizlist.no_of_quizes; i++)   printf("%d : %s\t no.of students attempted   %d\n",i+1,quizlist.quiz[i].name,quizlist.quiz[i].no_of_students_attempted);
     printf("0 : Create a quiz\n");
     int res;
     do {
-    scanf("%d",&res);
-    clearBuf();
-    if(res==0){
+	scanf("%d",&res);
+	clearBuf();
+  	if(res==0){
           addquiz();
     }
-    else if(res>0 && res <= quizlist.no_of_quizes){
+	else if(res>0 && res <= quizlist.no_of_quizes){
         admin_quizdetails(res-1);
-    }
+	}
     else if(res == -1) welcomepage_admin();
     else {
         printf("Not a valid response try agian\n");
         res = -2;
     }
 
-    }while(res <-1 || res> quizlist.no_of_quizes );
+	}while(res <-1 || res> quizlist.no_of_quizes );
 
 }
 
