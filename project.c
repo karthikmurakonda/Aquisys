267ea0da55ea6c25550bbbef0cee6856875c60d8
Om Patil <omsanjayp@gmail.com>
2021-05-30 17:28:42 +0530

    Added save to file feature

--- project.c
+++ project.c
@@ -2,11 +2,19 @@
 #include "project.h"
 #include "quiz.h"
 #include "admin.h"
-
+#include "file.h"
 
 int main() {
-    currentuser.ID = 0;
-    welcomepage_admin();
-    takeQuiz(0);
+    appdata_read();
+    printf("Choose what to do:\n1: Admin Page\n2: Quiz\n");
+    int com = scanf_int();
+    if (com==1) {
+        welcomepage_admin();
+    }
+    else if (com==2) {
+        currentuser.ID=0;
+        takeQuiz(0);
+    }
+    appdata_save();
     return 0;
 }
