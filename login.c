28c3ebd178b93188332cf6a8d6a82496dc528951
karthikmurakonda <karthik.murakonda14@gmail.com>
2021-06-01 22:41:40 +0530

    fixed few bugs in login.c
    1.expelled go to statement
    2.fixed few bugs

--- login.c
+++ login.c
@@ -14,10 +14,12 @@ void login() {
     printf("\n\t\t\t\t---------------------------");
     printf("\n\n\t\t\t\t  ENTER USERNAME: ");
     scanf("%s", username);
+    clearBuf();
     printf("\n\n\t\t\t\t  ENTER PASSWORD: ");
     scanf("%s", password);
+    clearBuf();
     for (i = 0; i < max_users; i++) {
-        if (userlist[i].username != 0) {
+        if (userlist[i].username[0] != 0) {
             checku = strcmp(username, userlist[i].username);
             checkp = strcmp(password, userlist[i].password);
             if (checku == 0 && checkp == 0) {
@@ -28,23 +30,30 @@ void login() {
                 currentuser.type=userlist[i].type;
                 status=0;
                 clearscr();
-                break;
-            } else if (checku == 0 && checkp != 0) {
+                return;
+            }
+            else if (checku == 0 && checkp != 0) {
                 printf("\n\n\n\t\t\tWRONG PASSWORD");
-                printf("\n\n\t\t\t\t  (Press 'Y' to re-login)");
-                if (getchar() == 'y' || getchar() == 'Y')
+                printf("\n\n\t\t\t\t  (Press 'Y' to re-login)\n");
+                do{
+                char res = getchar();
+                clearBuf();
+                if (res == 'y' || res == 'Y'){
                     login();
+                    return;
+                }
+                printf("Invalid response try again.\n");         
+                }while(1);
             }
         }
-        else {
-            break;
-        }
     }
-    if (status) {
-        printf("No such user found!\n");
+        printf("No such user found!\npress ENTER to go back\n");
+        getchar();
+        clearBuf();
+        loginpage();
+        return;
     }
-    getchar();
-}
+
 
 void loginpage() {
     int n;
@@ -54,7 +63,7 @@ void loginpage() {
     printf("\n\n\n\n\t\t\tPress Enter to proceed...!!\n");
     if (getchar() == 13)
         clearscr();
-    XY:
+    do{
         printf("\n\n\n\t\t\t1. LOGIN\t\t2. EXIT");
     printf("\n\n\n\t\t\t\tENTER YOUR CHOICE: ");
     scanf("%d", & n);
@@ -62,15 +71,15 @@ void loginpage() {
     case 1:
         clearscr();
         login();
-        break;
+        return;
     case 2:
         clearscr();
         exit(1);
-        break;
+        return;
     default:
         printf("\n\n\t\t\tPress Enter to re-Enter the choice");
         if (getchar() == 13)
             clearscr();
-        goto XY;
     }
+    }while(1);
 }
