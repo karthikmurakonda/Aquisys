58748517651a06b876f4da11efa3b3c711d9ed4c
Om Patil <omsanjayp@gmail.com>
2021-06-02 01:01:16 +0530

    Added appdata initialization

--- file.c
+++ file.c
@@ -16,6 +16,7 @@ void appdata_save() {
 
 	fwrite(&quizlist, sizeof(struct Quizlist), 1, datafile);
 	fwrite(&userlist, sizeof(struct User), max_users, datafile);
+	fclose(datafile);
 }
 
 void appdata_read() {
@@ -25,12 +26,29 @@ void appdata_read() {
 	datafile = fopen(".appdata.dat", "r");
 
 	//Check if open
-	if (datafile == NULL)
-	{
-		printf("\nError 2.1: Could not open appdata\n");
-		exit (1);
+	if (datafile == NULL) {
+		printf("Is this the first time you have started the quiz application?\n[Answer 'y' for yes and 'n' for no]\n");
+		char com;
+		scanf("%c", &com);
+		clearBuf();
+		if (com=='y'||com=='Y') {
+			//Make empty appdata file
+			datafile = fopen(".appdata.dat", "w");
+			fclose(datafile);
+			//Then try again
+			appdata_read();
+		}
+		else if (com=='n'||com=='N') {
+			printf("Error!\nAppdata not found!");
+			exit(1);
+		}
+		else {
+			printf("Invalid option!\nPlease try again,");
+			appdata_read();
+		}
 	}
-
-	fread(&quizlist, sizeof(struct Quizlist), 1, datafile);
-	fread(&userlist, sizeof(struct User), max_users, datafile);
+	else {
+		fread(&quizlist, sizeof(struct Quizlist), 1, datafile);
+		fread(&userlist, sizeof(struct User), max_users, datafile);
+	}
 }
